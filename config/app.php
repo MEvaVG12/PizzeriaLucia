76a4b494e884b4c01356192a862c1a363e9a6d8d
Eva <villarrealguzman@gmail.com>
2017-03-29 17:00:22 -0300

    Agregado de paquete entrust, creación del modelo, creación de migraciones y creación de seed relacionadas

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+       /*
+         * Package Service Entrust...
+         */
+        Zizaco\Entrust\EntrustServiceProvider::class,
+
     ],
 
     /*
@@ -225,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
 
     ],
 

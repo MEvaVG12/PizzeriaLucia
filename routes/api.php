368569d87af6a345f7912600b548eb48c88cd193
MicaelaM <mica.montenegro.sistemas@gmail.com>
2017-05-03 08:12:32 -0300

    route/api

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,5 @@
 <?php
-
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -12,25 +10,22 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-
 Route::post('/promotion/create', 'PromotionController@store');
 //Route::POST('/promotion/create', function() { });
-
 Route::put('stock/update/{id}','StockController@update');
 Route::get('stocks', function(){
   return Datatables::eloquent(App\Stock::select('stocks.id', 'stocks.ingredient_id', 'stocks.amount', 'ingredients.name as name')
             ->join('ingredients','ingredients.id','=','stocks.ingredient_id'))->make(true);
 });
 Route::get('products','ProductController@showProducts');
+Route::post('promotion/{id}/products','PromotionController@showProducts');
 //Route::get('stocks','StockController@showStock');
 Route::post('ingredients','ProductController@showIngredients');
-
 Route::get('promotion/index','PromotionController@showPromotions');
 Route::post('promotion/index/promotionDetails','PromotionController@showPromotionDetails');
 Route::put('product/update/{id}','ProductController@update');
 Route::put('promotion/delete/{id}','PromotionController@destroy');
+Route::put('promotion/update/{id}','PromotionController@update');
